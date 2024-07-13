6def76037a29b2e90e890abb2ee8236639251307
zmertens <zach.mm035@gmail.com>
2024-07-13 08:11:45 -0700

    Update with latest maze builder JS stuff, add polling function to enable download button

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/be13537b-f604-447a-beaf-a6d63496fb8e/deploy-status)](https://app.netlify.com/sites/jade-semifreddo-f24ef0/deploys?branch=main)
+
 # maze-builder-frontend
 
-Deployed on Netlify - distribute securely using `netlify.toml`.
+Deployed on [Netlify](https://668145265561420d1369a177--jade-semifreddo-f24ef0.netlify.app/) - distribute securely using `netlify.toml`:
+```toml
+[[headers]]
+  for = "/*"
+  [headers.values]
+    Cross-Origin-Opener-Policy = "same-origin"
+    Cross-Origin-Embedder-Policy = "require-corp"
+```
 
 Public files are generated using [https://www.github.com/zmertens/MazeBuilder](https://www.github.com/zmertens/MazeBuilder).
+The steps to spin up the frontend is a multi-step process:
+1. Generate JavaScript and WebAssembly files from MazeBuilder's C++ codebase. This is done with Emscripten toolchain and CMake.
+   - [https://github.com/zmertens/MazeBuilder/blob/dev/README.md#cmake](https://github.com/zmertens/MazeBuilder/blob/dev/README.md#cmake)
+2. Put the generated JS/WASM files in the frontend repo under the `/public/` folder.
+   - [https://github.com/zmertens/maze-builder-frontend/tree/main/public](https://github.com/zmertens/maze-builder-frontend/tree/main/public)
+3. From the root of the frontend source code, run `npm i` and then `npm run dev`.
+   1. This should start the Vite server, open a browser with the specified port. For example, `localhost:5173`
+4. The application should be running in the browser.
+
+Additionally, one can run a local Python server directly from MazeBuilder's C++ repo using `secure_python_server.py`.
+  - [https://github.com/zmertens/MazeBuilder/blob/dev/secure_http_server.py](https://github.com/zmertens/MazeBuilder/blob/dev/secure_http_server.py)
--- src/components/MazeBuilderComponent.tsx
+++ src/components/MazeBuilderComponent.tsx
@@ -3,10 +3,52 @@ import React, { useEffect, useState } from 'react';
 const MazeBuilderComponent = () => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [isModuleReady, setIsModuleReady] = useState(false);
+  const [mazeInfo, setMazeInfo] = useState(null);
+
+  let intervalId: number = -1;
+
+  const pollForMazeData = () => {
+    if (window.Module && window.Module.craft) {
+      try {
+        let instance = new window.Module.craft("Maze Builder", "Web", "NA");
+        if (!instance) {
+          console.error('Failed to create instance');
+          return;
+        }
+
+        // Polling for data readiness
+        intervalId = setInterval(() => {
+        if (instance.is_json_rdy()) {
+          const mazeInfoStr = instance.get_json();
+          if (mazeInfoStr) {
+            const mazeInfo = JSON.parse(mazeInfoStr);
+            // Check if user create a unique maze name
+            if (mazeInfo.name[0] !== '.') {
+              setMazeInfo(mazeInfo);
+              setIsModuleReady(true);
+              console.log('Maze data is ready:', mazeInfo.name);
+              console.log('Maze data:', mazeInfo.data);
+              // Stop polling, got the data
+              clearInterval(intervalId);
+            }
+          }
+          instance.delete();
+        }
+      }, 1000); // Check every 1 second
+      } catch (error) {
+        console.error('Error creating instance:', error);
+      }
+    }
+   }; // pollForMazeData
 
   useEffect(() => {
     loadWASM();
     
+    // Poll every 1 second to check if the maze data is ready
+    const intervalId = setInterval(() => {
+      pollForMazeData();
+    }, 1000);
+
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
     };
@@ -14,7 +56,10 @@ const MazeBuilderComponent = () => {
     window.addEventListener('resize', handleResize);
 
     // Cleanup function to remove the event listener
-    return () => window.removeEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      clearInterval(intervalId);
+    }
   }, []);
 
   const loadWASM = async () => {
@@ -23,30 +68,64 @@ const MazeBuilderComponent = () => {
       document.body.appendChild(script);
   
       // This was pretty tricky because of how React handles component lifecycles
-      // WASM files are asynchronous, so we need to wait for the script to load
+      // WASM files are asynchronous, so we need to load the script asynchronously
       script.onload = async () => {
         window.Module = await Module({ canvas : document.getElementById("canvas")});
         setIsModuleReady(true);
-        }
+      }
    }; // loadWASM
 
-  const handleDownloadClick = () => {
+   const handleDownloadClick = () => {
     if (!isModuleReady) {
-        console.error('Module is not ready');
-        return;
+      console.error('Module is not ready');
+      return;
     }
-    const mazeInfoStr = window.Module.do_stuff();
-    const mazeInfo = JSON.parse(mazeInfoStr);
-    console.log("Maze Name:", mazeInfo.name);
-    console.log("Maze Data:", mazeInfo.data);
-  };
+  
+    if (window.Module && window.Module.craft) {
+      try {
+        let instance = new window.Module.craft("Maze Builder", "Web", "NA");
+        if (!instance) {
+          console.error('Failed to create instance');
+          return;
+        }
+  
+        // Polling for data readiness
+        intervalId = setInterval(() => {
+          if (instance.is_json_rdy()) {
+            clearInterval(intervalId); // Stop polling
+            const mazeInfoStr = instance.get_json();
+            const mazeInfo = JSON.parse(mazeInfoStr);
+            console.log("Maze Name:", mazeInfo.name);
+            console.log("Maze Data:", mazeInfo.data);
+            setMazeInfo(mazeInfo);
+            // Proceed with creating a download button for the JSON
+            const dataStr = JSON.stringify(mazeInfo.data);
+            const blob = new Blob([dataStr], { type: 'application/json' });
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = `${mazeInfo.name}`;
+            document.body.appendChild(a);
+            a.click(); // Trigger download
+            document.body.removeChild(a); // Cleanup
+            URL.revokeObjectURL(url); // Free up memory
+          }
+        }, 1000); // Check every 100 milliseconds
+        instance.delete();
+      } catch (error) {
+        console.error('Error creating instance:', error);
+      }
+    } else {
+      console.error('Module.craft is not available');
+    }
+  }; // handleDownloadClick
 
   return (
       <div>
         <h1> Build and Download Mazes</h1>
         <canvas id="canvas" width={windowWidth} style={{ backgroundColor : 'blue'}} onContextMenu={ (event) => event.preventDefault() } />            
         <br />
-        <button onClick={handleDownloadClick} disabled={!isModuleReady} >Download Maze</button>
+        <button onClick={handleDownloadClick} disabled={!isModuleReady || !mazeInfo} >Download Maze</button>
       </div>
   );
 
